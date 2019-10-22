4c7b4755e0f63eff25dcd905878fa65ccd7d81f8
Clio Lang <54592031+cliolang@users.noreply.github.com>
2019-10-22 12:34:15 -0400

    Add files via upload

---
+++ KingdomDriver.java
@@ -0,0 +1,19 @@
+package csce247.assignments.adapter;
+
+public class KingdomDriver {
+
+	public static void main(String[] args) {
+		Warrior warrior1 = new Knight();
+		Wizard sorcerer = new Sorcerer();
+		Warrior warrior2 = new WizardAdapter(sorcerer);
+		
+		System.out.println("***** OFFENSE *****");
+		warrior1.attack();
+		warrior2.attack();
+		
+		System.out.println("\n***** Defense *****");
+		warrior1.defend();
+		warrior2.defend();
+	}
+
+}
