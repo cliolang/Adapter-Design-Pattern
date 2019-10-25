8b435ff0a5ca9f40809f755cd3c62b5e216baf6e
landin3thorstedUSC <55707464+landin3thorstedUSC@users.noreply.github.com>
2019-10-25 12:42:32 -0400

    Add files via upload
    Adding the Warrior interface and the Knight class that implements it

---
+++ Knight.java
@@ -0,0 +1,23 @@
+/*
+ * portiaportia(1)
+ * Class that implements the warrior interface
+ */
+public class Knight implements Warrior {
+
+  /*
+   * Method that prints out that Knight is attacking
+   */
+  @Override
+  public void attack() {
+    System.out.println("Attacking: I'm bringing you down!");
+  }
+  
+  /*
+   * Method that prints out that Knight is defending
+   */
+  @Override
+  public void defend() {
+    System.out.println("Defending: We must protect the castle");
+  }
+
+}
---
+++ Warrior.java
@@ -0,0 +1,8 @@
+/*
+ * portiaportia(1)
+ * Outline for the classes that implement Warrior
+ */
+public interface Warrior {
+  public void attack();
+  public void defend();
+}
